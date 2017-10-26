62ef3c2845a3d03707750fcc82d744f34094ed1f
Luiza de Azambuja Hagemann <luiza.hagemann@corp.globo.com>
2017-10-26 09:39:24 -0200

    directives conversion mitsuba -> pbrt working

--- src/mitsuba/convertTo.py
+++ src/mitsuba/convertTo.py
@@ -4,7 +4,6 @@ import mitsuba as mit
 import mitsubaToPBRT as mtpbrt
 import numpy as np
 
-
 def toPBRT(scene):
     np.set_printoptions(suppress=True)
     
@@ -14,17 +13,17 @@ def toPBRT(scene):
         if scene.integrator:
             outfile.write('Integrator ')
 
-            int_type = mtpbrt.integratorType[scene.integrator.int_type]
-            if int_type:
+            if scene.integrator.int_type in mtpbrt.integratorType:
+                int_type = mtpbrt.integratorType[scene.integrator.int_type]
                 outfile.write('"' + int_type + '" ')
             else:
                 outfile.write('"path" ')
 
             for param in scene.integrator.params:
-                pbrt_param = mtpbrt.integratorParam[param.name]
-                if pbrt_param:
+                if param.name in mtpbrt.integratorParam:
+                    pbrt_param = mtpbrt.integratorParam[param.name]
                     outfile.write('"' + param.val_type + ' ' + pbrt_param + '" ')
-                    outfile.write('[' + param.value + ']')
+                    outfile.write('[ ' + param.value + ' ]')
 
             outfile.write('\n')
 
@@ -36,11 +35,11 @@ def toPBRT(scene):
             # convert transform matrix to inverse transpose (PBRT default)
             m = scene.sensor.transform.matrix
             m_T = np.transpose(m)
-            m_IT = np.linalg.inv(m_t)
+            m_IT = np.linalg.inv(m_T)
 
             for i in range(0,4):
                 for j in range(0,4):
-                    outfile.write(m_IT[i][j])
+                    outfile.write(str(m_IT[i][j]))
                     outfile.write(' ')
 
             outfile.write(']')
@@ -50,42 +49,32 @@ def toPBRT(scene):
         if scene.sensor.sampler:
             outfile.write('Sampler ')
 
-            sampler_type = mtpbrt.samplerType[scene.sensor.sampler.sampler_type]
-            if sampler_type
+            if scene.sensor.sampler.sampler_type in mtpbrt.samplerType:
+                sampler_type = mtpbrt.samplerType[scene.sensor.sampler.sampler_type]
                 outfile.write('"' + sampler_type + '" ')
             else:
                 outfile.write('"sobol" ')
 
             for param in scene.sensor.sampler.params:
-                pbrt_param = mtpbrt.samplerParam[param.name]
-                if pbrt_param:
+                if param.name in mtpbrt.samplerParam:
+                    pbrt_param = mtpbrt.samplerParam[param.name]
                     outfile.write('"' + param.val_type + ' ' + pbrt_param + '" ')
-                    if pbrt_param.val_type is 'string':
+                    if param.val_type is 'string':
                         outfile.write('[ "' + param.value + '" ]')
                     else:
-                        outfile.write('[' + param.value + ']')
+                        outfile.write('[ ' + param.value + ' ]')
             
             outfile.write('\n')
 
         # filter
-        if scene.sensor.filter:
+        if scene.sensor.film.filter_type:
             outfile.write('PixelFilter ')
 
-            filter_type = mtpbrt.filterType[scene.sensor.filter.filter_type]
-            if filter_type:
+            if scene.sensor.film.filter_type in mtpbrt.filterType:
+                filter_type = mtpbrt.filterType[scene.sensor.film.filter_type]
                 outfile.write('"' + filter_type + '" ')
             else:
                 outfile.write('"triangle" ')
-
-
-            for param in scene.sensor.filter.params:
-                pbrt_param = mtpbrt.filterParam[param.name]
-                if pbrt_param:
-                    outfile.write('"' + param.val_type + ' ' + pbrt_param + '" ')
-                    if pbrt_param.val_type is 'string':
-                        outfile.write('[ "' + param.value + '" ]')
-                    else:
-                        outfile.write('[' + param.value + ']')
     
             outfile.write('\n')
 
@@ -93,42 +82,44 @@ def toPBRT(scene):
         if scene.sensor.film:
             outfile.write('Film ')
 
-            film_type = mtpbrt.filmType[scene.sensor.film.film_type]
-            if film_type:
+            if scene.sensor.film.film_type in mtpbrt.filmType:
+                film_type = mtpbrt.filmType[scene.sensor.film.film_type]
                 outfile.write('"' + film_type + '" ')
             else:
                 outfile.write('"image" ')
 
 
             for param in scene.sensor.film.params:
-                pbrt_param = mtpbrt.filmParam[param.name]
-                if pbrt_param:
+                if param.name == 'fileFormat':
+                    outfile.write('"string filename" [ "scene.' + param.value + '" ]' )
+                elif param.name in mtpbrt.filmParam:
+                    pbrt_param = mtpbrt.filmParam[param.name]
                     outfile.write('"' + param.val_type + ' ' + pbrt_param + '" ')
-                    if pbrt_param.val_type is 'string':
-                        outfile.write('[ "' + param.value + '" ]')
+                    if param.val_type is 'string':
+                        outfile.write('[ "' + param.value + '" ] ')
                     else:
-                        outfile.write('[' + param.value + ']')
-    
+                        outfile.write('[ ' + param.value + ' ] ')
+
             outfile.write('\n')
 
         # sensor/camera
         if scene.sensor:
             outfile.write('Camera ')
 
-            sensor_type = mtpbrt.sensorType[scene.sensor.sensor_type]
-            if sensor_type:
+            if scene.sensor.sensor_type in mtpbrt.sensorType:
+                sensor_type = mtpbrt.sensorType[scene.sensor.sensor_type]
                 outfile.write('"' + sensor_type + '" ')
             else:
                 outfile.write('"perspective" ')
 
             for param in scene.sensor.params:
-                pbrt_param = mtpbrt.sensorParam[param.name]
-                if pbrt_param:
+                if param.name in mtpbrt.sensorParam:
+                    pbrt_param = mtpbrt.sensorParam[param.name]
                     outfile.write('"' + param.val_type + ' ' + pbrt_param + '" ')
-                    if pbrt_param.val_type is 'string':
+                    if param.val_type is 'string':
                         outfile.write('[ "' + param.value + '" ]')
                     else:
-                        outfile.write('[' + param.value + ']')
+                        outfile.write('[ ' + param.value + ' ]')
 
             outfile.write('\n')
 
@@ -143,7 +134,11 @@ def toPBRT(scene):
         outfile.write('WorldEnd\n')
 
 
+def main():
+    scene = mit.read_from_xml('/Users/luiza.hagemann/Development/pbr_scene_converter/test_files/mitsuba/classroom.xml')
+    toPBRT(scene)
 
+if  __name__ =='__main__': main()
 
 
 
