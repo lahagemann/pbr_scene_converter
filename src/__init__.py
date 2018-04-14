fb2576ca3e7e11bf5fccc87426a7894275017b89
lahagemann <lahagemann@inf.ufrgs.br>
2018-04-14 11:48:10 -0300

    fix __init__.py deletion

---
+++ src/__init__.py
@@ -0,0 +1,48 @@
+from mitsuba import MitsubaToPBRTv3 as mp
+from mitsuba import MitsubaToLuxRender as ml
+from core import MitsubaLoader as mit
+
+import sys
+
+if __name__ == '__main__':
+	filename = ''
+	source = ''
+	destination = ''
+	output = 'scene'
+
+	if len(sys.argv) <= 1:
+		print 'Please call PBR Scene converter using the following parameters: -s [source renderer] -d [destination render] -f [input filename] <option: -o [output filename]>'
+	elif not '-f' in sys.argv:
+		print 'No input file specified. Please call PBR Scene converter using the following parameters: -s [source renderer] -d [destination render] -f [input filename] -o [output filename]'
+	elif not '-s'in sys.argv:
+		print 'No source renderer specified. Please call PBR Scene Converter with the following parameters: -s [source renderer] -d [destination render] -f [input filename] -o [output filename]'
+	
+	else:
+		for i in range(1,len(sys.argv)):
+			if sys.argv[i] == '-s':
+				source = sys.argv[i+1]
+
+			elif sys.argv[i] == '-d':
+				destination = sys.argv[i+1]
+
+			elif sys.argv[i] == '-f':
+				filename = sys.argv[i+1]
+
+			elif sys.argv[i] == '-o':
+				output = sys.argv[i+1]
+
+		# load scene from file
+		if source == 'mitsuba':
+			loader = mit.MitsubaLoader(filename)
+
+		else:
+			loader = mit.MitsubaLoader(filename)
+
+		# convert
+		if destination == 'pbrt':
+			mp.MitsubaToPBRTv3(loader.scene, output)
+			if not output.endswith('.pbrt'):
+				output += '.pbrt'
+		elif destination == 'lux' or destination == 'luxrender':
+			ml.MitsubaToLuxRender(loader.scene, output)
+
